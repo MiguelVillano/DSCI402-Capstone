7f710c3a4a81a920f3e16a601d731c772bf11d84
MiguelVillano <MiguelVillanoRH@gmail.com>
2026-02-23 13:25:10 -0500

    Fixed Data Preprocessing, Added Test Train Split

--- Miguel/LoanDefaultPrediction.py
+++ Miguel/LoanDefaultPrediction.py
@@ -1,13 +1,10 @@
 import marimo
 
-__generated_with = "0.19.2"
+__generated_with = "0.19.7"
 app = marimo.App(width="medium")
 
 
 @app.cell
-<<<<<<< HEAD
-def _():
-=======
 def _(mo):
     mo.md(r"""
     TO DO NEXT TIME:
@@ -21,39 +18,18 @@ def _(mo):
 
 
 @app.cell
-def load_libraries():
-    import subprocess, sys
-
-    # packages we need for this project
-    packages = [
-        "marimo",
-        "pandas",
-        "numpy",
-        "matplotlib",
-        "seaborn",
-        "scikit-learn",
-    ]
-    
-    # import each package, but if there is an error than it will pip install
-    for pkg in packages:
-        try:
-            __import__(pkg if pkg != "scikit-learn" else "sklearn")
-        except ImportError:
-            subprocess.check_call([sys.executable, "-m", "pip", "install", pkg])
-
-    # import
->>>>>>> 75647186436a72a89604480adb59c2da401a7b94
+def _():
     import marimo as mo
     import pandas as pd
     import numpy as np
     import matplotlib.pyplot as plt
     import seaborn as sns
-<<<<<<< HEAD
 
     #Logistic Regression
     from sklearn import metrics
     from sklearn.linear_model import LogisticRegression
-    return LogisticRegression, metrics, mo, pd, plt
+    from sklearn.model_selection import train_test_split
+    return LogisticRegression, metrics, mo, pd, plt, train_test_split
 
 
 @app.cell
@@ -69,12 +45,6 @@ def _(mo):
     START ON NAIVE BAYES
     """)
     return
-=======
-    from sklearn import preprocessing
-
-    # return the abbreviations
-    return mo, pd, np, plt, sns, preprocessing
->>>>>>> 75647186436a72a89604480adb59c2da401a7b94
 
 
 @app.cell
@@ -123,7 +93,7 @@ def _(mo):
 def _(pd):
     path = '../train.csv'
     df = pd.read_csv(path)
-    return (df,)
+    return df, path
 
 
 @app.cell
@@ -263,26 +233,26 @@ def _(mo):
 
 
 @app.cell
-def _(df):
-    value = df.annual_income
+def _():
+    #value = df.annual_income
 
     # Calculate quartiles
-    Q1 = value.quantile(0.25)
-    Q3 = value.quantile(0.75)
-    IQR = Q3 - Q1
+    #Q1 = value.quantile(0.25)
+    #Q3 = value.quantile(0.75)
+    #IQR = Q3 - Q1
 
     # Calculate Tukey's fences
-    lower_bound = Q1 - 1.5 * IQR
-    upper_bound = Q3 + 1.5 * IQR
+    #lower_bound = Q1 - 1.5 * IQR
+    #upper_bound = Q3 + 1.5 * IQR
 
-    print(f"Lower Bound (Tukey's Fence): {lower_bound}")
-    print(f"Upper Bound (Tukey's Fence): {upper_bound}")
+    #print(f"Lower Bound (Tukey's Fence): {lower_bound}")
+    #print(f"Upper Bound (Tukey's Fence): {upper_bound}")
 
     # Identify outliers using Tukey's fences
-    tukey_outliers = df[(value < lower_bound) | (value > upper_bound)]
+    #tukey_outliers = df[(value < lower_bound) | (value > upper_bound)]
 
-    print("\nOutliers (Tukey's Fences):")
-    print(tukey_outliers)
+    #print("\nOutliers (Tukey's Fences):")
+    #print(tukey_outliers)
     return
 
 
@@ -325,11 +295,18 @@ def _(df):
 
 
 @app.cell
+def _(pd):
+    pd.set_option('future.no_silent_downcasting', True)
+
+    return
+
+
+@app.cell
 def _(c_df):
     c_df["gender"] = c_df["gender"].replace({
         "Male": 0,
         "Female": 1,
-        "Other": 3
+        "Other": 2
     })
 
     c_df['marital_status'] = c_df["marital_status"].replace({
@@ -406,6 +383,14 @@ def _(c_df):
     return
 
 
+@app.cell
+def _(mo):
+    mo.md(r"""
+    Initially, I believed that there was not much difference in grouping the subgrades together against leaving them separate. Later, this will be tested out through the first logistic regression test.
+    """)
+    return
+
+
 @app.cell(hide_code=True)
 def _(mo):
     mo.md(r"""
@@ -423,104 +408,15 @@ def _(mo):
 
 
 @app.cell
-def _(c_df):
-    X_train = c_df.drop(columns=["loan_paid_back"])
-    return (X_train,)
+def _(c_df, train_test_split):
+    train_df, test_df = train_test_split(c_df, test_size=0.2, random_state=42)
+    X_train = train_df.drop(columns=["loan_paid_back"])
+    Y_train = train_df["loan_paid_back"]
 
+    X_test = test_df.drop(columns=["loan_paid_back"])
+    Y_test = test_df["loan_paid_back"]
 
-@app.cell
-def _(c_df):
-
-    Y_train = c_df["loan_paid_back"]
-    return (Y_train,)
-
-
-@app.cell
-def _(pd):
-    #Defining Test Dataframe and Processing it.
-    path2 = '../test.csv'
-    t_df = pd.read_csv(path2)
-
-    t_df["gender"] = t_df["gender"].replace({
-        "Male": 0,
-        "Female": 1,
-        "Other": 3
-    })
-
-    t_df['marital_status'] = t_df["marital_status"].replace({
-        "Single": 0,
-        "Married": 1,
-        "Divorced": 2,
-        "Widowed": 3
-    })
-
-    t_df['education_level'] = t_df["education_level"].replace({
-        "High School": 0,
-        "Bachelor's": 1,
-        "Master's": 2,
-        "PhD": 3,
-        "Other":4
-    })
-    t_df['employment_status'] = t_df["employment_status"].replace({
-        "Unemployed": 0,
-        "Employed": 1,
-        "Self-employed": 2,
-        "Retired": 3,
-        "Student":4
-    })
-    t_df['loan_purpose'] = t_df["loan_purpose"].replace({
-        "Debt consolidation": 0,
-        "Home": 1,
-        "Education": 2,
-        "Vacation": 3,
-        "Car": 4,
-        "Medical": 5,
-        "Business": 6,   
-        "Other": 7,
-    })
-    t_df["grade_subgrade"] = t_df["grade_subgrade"].replace({
-        "F1": 0,
-        "F2": 0,
-        "F3": 0,
-        "F4": 0,
-        "F5": 0,
-        "E1": 1,
-        "E2": 1,
-        "E3": 1,
-        "E4": 1,
-        "E5": 1,
-        "D1": 2,
-        "D2": 2,
-        "D3": 2,
-        "D4": 2,
-        "D5": 2,
-        "C1": 3,
-        "C2": 3,
-        "C3": 3,
-        "C4": 3,
-        "C5": 3,
-        "B1": 4,
-        "B2": 4,
-        "B3": 4,
-        "B4": 4,
-        "B5": 4,
-        "A1": 5,
-        "A2": 5,
-        "A3": 5,
-        "A4": 5,
-        "A5": 5
-    })
-
-    t_df
-    return (t_df,)
-
-
-@app.cell
-def _(t_df):
-    X_test = t_df.drop(columns=["loan_paid_back"])
-
-    Y_test = t_df["loan_paid_back"]
-    return
+    return X_test, X_train, Y_test, Y_train
 
 
 @app.cell
@@ -549,7 +445,158 @@ def _(LogisticRegression, X_train, Y_train, metrics):
     print(logregCnf_matrix)
     print(logregScore)
 
-    print("Using a simple logistic regression with default settings, we achieve an 82% accuracy using our training data.")
+    print("Using a simple logistic regression with default settings, we achieve an", logregScore ,"accuracy using our training data.")
+    return logreg, model
+
+
+@app.cell
+def _(X_test, Y_test, metrics, model):
+    # Using the Test Set
+
+    y_pred_Test = model.predict(X_test)
+
+    #Confusion Matrix for the Logistic Regression
+    logregCnf_matrix_Test = metrics.confusion_matrix(Y_test, y_pred_Test)
+    logregScore_Test = metrics.accuracy_score(Y_test, y_pred_Test)
+    print(logregCnf_matrix_Test)
+    print(logregScore_Test)
+
+    print("Using a simple logistic regression with default settings, we achieve an", logregScore_Test, " accuracy using our training data.")
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""
+    ### New Test for Checking if there is a viable difference between each separating the subgrades or not.
+    """)
+    return
+
+
+@app.cell
+def _(path, pd):
+    #Defining Test Dataframe and Processing it.
+    c2_df = pd.read_csv(path)
+
+    c2_df["gender"] = c2_df["gender"].replace({
+        "Male": 0,
+        "Female": 1,
+        "Other": 3
+    })
+
+    c2_df['marital_status'] = c2_df["marital_status"].replace({
+        "Single": 0,
+        "Married": 1,
+        "Divorced": 2,
+        "Widowed": 3
+    })
+
+    c2_df['education_level'] = c2_df["education_level"].replace({
+        "High School": 0,
+        "Bachelor's": 1,
+        "Master's": 2,
+        "PhD": 3,
+        "Other":4
+    })
+    c2_df['employment_status'] = c2_df["employment_status"].replace({
+        "Unemployed": 0,
+        "Employed": 1,
+        "Self-employed": 2,
+        "Retired": 3,
+        "Student":4
+    })
+    c2_df['loan_purpose'] = c2_df["loan_purpose"].replace({
+        "Debt consolidation": 0,
+        "Home": 1,
+        "Education": 2,
+        "Vacation": 3,
+        "Car": 4,
+        "Medical": 5,
+        "Business": 6,   
+        "Other": 7,
+    })
+    c2_df["grade_subgrade"] = c2_df["grade_subgrade"].replace({
+        "F1": 0,
+        "F2": 1,
+        "F3": 2,
+        "F4": 3,
+        "F5": 4,
+        "E1": 5,
+        "E2": 6,
+        "E3": 7,
+        "E4": 8,
+        "E5": 9,
+        "D1": 10,
+        "D2": 11,
+        "D3": 12,
+        "D4": 13,
+        "D5": 14,
+        "C1": 15,
+        "C2": 16,
+        "C3": 17,
+        "C4": 18,
+        "C5": 19,
+        "B1": 20,
+        "B2": 21,
+        "B3": 22,
+        "B4": 23,
+        "B5": 24,
+        "A1": 25,
+        "A2": 26,
+        "A3": 27,
+        "A4": 28,
+        "A5": 29
+    })
+
+    c2_df
+    return (c2_df,)
+
+
+@app.cell
+def _(c2_df, train_test_split):
+    train_df2, test_df2 = train_test_split(c2_df, test_size=0.2, random_state=42)
+    X_train2 = train_df2.drop(columns=["loan_paid_back"])
+    Y_train2 = train_df2["loan_paid_back"]
+
+    X_test2 = test_df2.drop(columns=["loan_paid_back"])
+    Y_test2 = test_df2["loan_paid_back"]
+    return X_test2, X_train2, Y_test2, Y_train2
+
+
+@app.cell
+def _(X_train2, Y_train2, logreg, metrics):
+
+    # fit the model with dat
+    model2 = logreg.fit(X_train2, Y_train2)
+
+    y_pred2 = model2.predict(X_train2)
+
+    #Confusion Matrix for the Logistic Regression
+    logregCnf_matrix2 = metrics.confusion_matrix(Y_train2, y_pred2)
+    logregScore2 = metrics.accuracy_score(Y_train2, y_pred2)
+    print(logregCnf_matrix2)
+    print(logregScore2)
+
+    print("Using a simple logistic regression with default settings, we achieve an", logregScore2 ,"accuracy using our training data.")
+    return (model2,)
+
+
+@app.cell
+def _(X_test2, Y_test2, metrics, model2):
+    y_pred2_test = model2.predict(X_test2)
+
+    #Confusion Matrix for the Logistic Regression
+    logregCnf_matrix2_test = metrics.confusion_matrix(Y_test2, y_pred2_test)
+    logregScore2_test = metrics.accuracy_score(Y_test2, y_pred2_test)
+    print(logregCnf_matrix2_test)
+    print(logregScore2_test)
+
+    print("Using a simple logistic regression with default settings, we achieve an", logregScore2_test ," accuracy using our training data.")
+    return
+
+
+@app.cell
+def _():
     return
 
 
